3b7c10fef9b9a3bd3ef37ccb18458b095a0f7db9
GurauR <robertogurau7@gmail.com>
2022-11-11 22:05:06 +0100

    Added Materia.jsx and Completed users.json

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Calendario from './pages/Calendario'
 import HomePage from './pages/HomePage'
+import Materia from './pages/Materia'
+
 
 export default function App() {
   return (
@@ -9,7 +11,8 @@ export default function App() {
     <Routes>
       <Route path='/' element={<Navigate to='/home'/>}/>
       <Route path='/home' element={<HomePage/>}/>
-      <Route path='/calendario' element={<Calendario/>}/>
+      <Route path='/calendario' element={<Calendario />} />S
+      <Route path='/materia' element={<Materia />} />
       <Route path='*' element={<h1>Qualcosa non va!</h1>}/>
     </Routes>
     </BrowserRouter>
